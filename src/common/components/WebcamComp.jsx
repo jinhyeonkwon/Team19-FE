705d2e1e172fba17180bec393d153f17f0b9920f
jinhyeonkwon <athexplorer@kaist.ac.kr>
2024-07-30 11:31:06 +0900

    refactor: capture button logic

--- src/common/components/WebcamComp.jsx
+++ src/common/components/WebcamComp.jsx
@@ -1,9 +1,9 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback } from 'react';
 
 import styled from 'styled-components';
 
 import Webcam from 'react-webcam';
-import StyledTypography from './StyledTypography';
+import CaptureButtonWithBackground from './CaptureButtonWithText';
 
 const WebcamWrapper = styled.div`
   width: 100%;
@@ -12,104 +12,6 @@ const WebcamWrapper = styled.div`
 
 const StyledWebcam = styled(Webcam)`
   object-fit: cover;
-`;
-
-/* Capture Button 관련 */
-const CaptureButtonWrapper = styled.button`
-  position: relative;
-  width: 124px;
-  height: 124px;
-  border-radius: 50%;
-  z-index: 1;
-`;
-
-const CaptureButtonPurpleCircle = styled.div`
-  position: absolute;
-  top: ${({ isClicked }) => (isClicked ? '5px' : '0px')};
-  left: 7px;
-  right: 7px;
-  width: 110px;
-  height: 110px;
-  border-radius: 50%;
-  background: ${({ theme, isClicked }) =>
-    isClicked ? theme.colors.PURPLE[900] : theme.colors.PURPLE[600]};
-  z-index: 3;
-`;
-
-const CaptureButtonWhiteCircle = styled.div`
-  position: absolute;
-  top: 0px;
-  left: 0px;
-  right: 0px;
-  width: 124px;
-  height: 124px;
-  border-radius: 50%;
-  background: ${({ theme }) => theme.colors.WHITE};
-  z-index: 2;
-`;
-
-const CaptureButtonImg = styled.img`
-  position: absolute;
-  top: ${({ isClicked }) => (isClicked ? '19px' : '14px')};
-  left: 21px;
-  right: 21px;
-  opacity: ${({ isClicked }) => (isClicked ? 0.6 : 1)};
-  z-index: 4;
-`;
-
-const CaptureButton = ({ onClick }) => {
-  const [isClicked, setIsClicked] = useState(false);
-
-  const handleMouseDown = () => {
-    setIsClicked(true);
-  };
-
-  const handleMouseUp = () => {
-    setIsClicked(false);
-  };
-
-  return (
-    <CaptureButtonWrapper
-      onClick={onClick}
-      onMouseDown={handleMouseDown}
-      onMouseUp={handleMouseUp}
-    >
-      <CaptureButtonPurpleCircle isClicked={isClicked} />
-      <CaptureButtonWhiteCircle />
-      <CaptureButtonImg
-        isClicked={isClicked}
-        src="/images/captureButtonImg.png"
-        alt="Capture button"
-      />
-    </CaptureButtonWrapper>
-  );
-};
-
-const CaptureButtonWithText = styled.div`
-  display: flex;
-  width: 124px;
-  flex-direction: column;
-  align-items: center;
-  gap: 9px;
-  z-index: 7;
-`;
-
-const CaptureButtonText = styled(StyledTypography)`
-  z-index: 10;
-`;
-
-const CaptureButtonBackground = styled.div`
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-  height: 249px;
-  background: rgba(0, 0, 0, 0.26);
-  backdrop-filter: blur(12.100000381469727px);
-  z-index: 1;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
 `;
 
 const WebcamTest = ({ setImageUrl }) => {
@@ -122,14 +24,7 @@ const WebcamTest = ({ setImageUrl }) => {
 
   return (
     <WebcamWrapper>
-      <CaptureButtonBackground>
-        <CaptureButtonWithText>
-          <CaptureButton onClick={capture} />
-          <CaptureButtonText type="24B" color="WHITE">
-            사진 찍기
-          </CaptureButtonText>
-        </CaptureButtonWithText>
-      </CaptureButtonBackground>
+      <CaptureButtonWithBackground capture={capture} />
       <StyledWebcam
         width="390px"
         height="797px"
